742982bb12b0c9e76521f6e5ce5e5635d31317ec
abowman32 <abowman@cloudbees.com>
2020-02-14 10:32:18 -0600

    Changed namespace for NFS client provisioner to kube-system

--- orc.sh
+++ orc.sh
@@ -113,7 +113,7 @@ kubectl get svc -n $ESNS
 
 # Change this to the correct FSADDR as needed 
 FSADDR=10.89.48.58
-
+kubectl config set-context $(kubectl config current-context) --namespace=kube-system
 helm install nfs-cp stable/nfs-client-provisioner --set nfs.server=${FSADDR} --set nfs.path=/volumes
 
 # Step 5: RWO storage
